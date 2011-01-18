cbc204966677cc2da16ac3aede1b4b8103b6ce8c
Matthew Garrett <mjg@redhat.com>
2011-01-18 09:43:21 -0500

    Comment the header

--- libbacklight.h
+++ libbacklight.h
@@ -17,12 +17,26 @@ struct backlight {
 	enum backlight_type type;
 };
 
-void backlight_destroy(struct backlight *backlight);
+/* 
+ * Find and set up a backlight for the given PCI device or card/connector
+ * combination. It is valid (and preferred) to provide both sets of information
+ */
 struct backlight *backlight_init(struct pci_device *dev, int card,
 				 int connector_type, int connector_type_id);
-long backlight_get_brightness(struct backlight *backlight);
+
+/* Free backlight resources */
+void backlight_destroy(struct backlight *backlight);
+
+/* Provide the maximum backlight value */
 long backlight_get_max_brightness(struct backlight *backlight);
+
+/* Provide the cached backlight value */
+long backlight_get_brightness(struct backlight *backlight);
+
+/* Provide the hardware backlight value */
 long backlight_get_actual_brightness(struct backlight *backlight);
+
+/* Set the backlight to a value between 0 and max */
 long backlight_set_brightness(struct backlight *backlight, long brightness);
 
 #ifdef __cplusplus
